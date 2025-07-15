d61e9c403795278f32d792bde3207d3c32a2fea9
teamprotechmain <teamprotech.main@gmail.com>
2025-07-15 16:56:43 +0800

    Added other pages and a modal

--- app.py
+++ app.py
@@ -7,5 +7,33 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/landing-pages')
+def landing_pages():
+    return render_template('landing-pages.html')
+
+@app.route('/about-me')
+def about_me():
+    return render_template('about-me.html')
+
+@app.route('/tools')
+def tools_used():
+    return render_template('tools.html')
+
+@app.route('/graphic-designs')
+def graphic_designs():
+    return render_template('graphic-designs.html')
+
+@app.route('/workflows')
+def workflows():
+    return render_template('workflows.html')
+
+@app.route('/services')
+def services():
+    return render_template('services.html')
+
+@app.route('/client-testimonials')
+def testimonials():
+    return render_template('testimonials.html')
+
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True)
